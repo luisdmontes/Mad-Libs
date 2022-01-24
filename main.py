4c139aa03061ddc9d6437ee55ea4e57dcd6daaca
Kousei Richeson <kousei.richeson@gmail.com>
2022-01-23 18:48:33 -0700

    Kousei Was Here

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# Kousei was here
+
 i = []
 
 def add(str):
@@ -14,26 +16,3 @@ add("a third friend in the group")
 
 # Story:
 print(f"It was a {i[0]} January day. Everyone woke up to the {i[1]} smell of {i[2]} cooking {i[3]} in the kitchen. \n \"Eat up everyone!\" {i[2]} said, serving everyone a big plate. {i[4]} sniffed it, then took a giant bite. \"{i[5]}\" {i[4]} yelled, \n immediately throwing up {i[6]} vomit all over {i[2]}'s face.")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
